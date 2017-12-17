9e62f6a9411116eda92ce2e7f887a170c1edabd0
Sanket Lopes <sanket.sl@samsung.com>
2017-12-17 10:19:20 -0600

    Block breaker assets

---
+++ BlockBreaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+	public int maxHits =1;
+	private int timesHit = 0;
+
+	private LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void OnCollisionEnter2D(Collision2D collision2d){
+		Debug.Log ("Collision : " + ++timesHit);
+		SimulateWin ();
+	}
+
+	void OnTriggerEnter2D(Collider2D collider2d){
+		Debug.Log ("Trigger");
+	}
+
+	void SimulateWin(){
+		levelManager.LoadNextLevel ();
+	}
+}
+	
